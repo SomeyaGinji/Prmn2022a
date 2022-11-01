832c6f060b68fcaf9b7188bfedbc8d6f11a345e3
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-01 21:19:00 +0900

    Exercise2_2クラスを作成

--- src/lecture2/Exercise2_2.java
+++ src/lecture2/Exercise2_2.java
@@ -4,6 +4,13 @@ public class Exercise2_2 {
 
     public static void main(String[] args) {
 
+        Car car = new Car();
+        GasStation gasStation = new GasStation();
+
+        car.run(); //給油前
+        gasStation.refuel(car); //給油する
+        car.run(); //給油後
+
     }
 
 }
