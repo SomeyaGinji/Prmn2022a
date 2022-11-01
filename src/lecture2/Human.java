67e7e9623c2202e8d20ac49d34fb9aadbfd9d4e7
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-01 20:58:31 +0900

    Humanクラスを修正

--- src/lecture2/Human.java
+++ src/lecture2/Human.java
@@ -6,20 +6,21 @@ public class Human {
     String name;
     int age;
 
-    //コンストラクタ
+    //コンストラクタ（インスタンス化と同時に呼び出されるメソッド）
     Human(String name, int age){
-        this.name = name;
+        this.name = name; //コンストラクタの引数をフィールド変数に当てはめる
         this.age = age;
     }
 
     //メソッド
     void print(){
-        System.out.println(name + " " + age + "歳です。");
+
         if (age <= 18) {
-            System.out.println("生徒です。");
+            System.out.println("生徒:" + name + " " + age + "歳です。");
         } else if (age <= 22) {
-            System.out.println("学生です。");
+            System.out.println("学生:" + name + " " + age + "歳です。");
         }
+
     }
 
 }
