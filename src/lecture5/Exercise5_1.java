22feae2cd7a8f2c4ef15b802974f5c9582e2d5e9
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-21 14:01:37 +0900

    課題1を終了

---
+++ src/lecture5/Exercise5_1.java
@@ -0,0 +1,14 @@
+package lecture5;
+
+public class Exercise5_1 {
+
+    public static void main(String[] args) {
+
+        // Insectクラスをインスタンス化
+        Insect insect = new Insect();
+
+        // Insectクラスのmoveメソッドを呼び出す
+        insect.move();
+
+    }
+}
---
+++ src/lecture5/Insect.java
@@ -0,0 +1,9 @@
+package lecture5;
+
+public class Insect {
+
+    // メソッド
+    public void move() {
+        System.out.println("歩いたよ。");
+    }
+}
