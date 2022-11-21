fb4efa701ca6019d5bd713b1769ce7844f352217
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-21 14:26:01 +0900

    課題5を終了

---
+++ src/lecture5/Exercise5_5.java
@@ -0,0 +1,14 @@
+package lecture5;
+
+public class Exercise5_5 {
+
+    public static void main(String[] args) {
+
+        // LocustのインスタンスをInsect型の変数に代入
+        Insect insect = new Locust();
+
+        // Locustのmoveメソッドを呼び出す
+        insect.move();
+
+    }
+}
