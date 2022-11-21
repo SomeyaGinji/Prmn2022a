6373fb70827cd8c9ea4905677cb8893d8653f8f5
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-21 14:28:22 +0900

    課題3を修正

--- src/lecture5/Exercise5_3.java
+++ src/lecture5/Exercise5_3.java
@@ -4,11 +4,11 @@ public class Exercise5_3 {
 
     public static void main(String[] args) {
 
-        // Insect型の変数insectにButterflyクラスのインスタンスを代入
-        Insect insect = new Butterfly();
+        // Insect型の変数swallowtail(アゲハ蝶)にButterflyクラスのインスタンスを代入
+        Insect swallowtail = new Butterfly();
 
         // Butterflyクラスのmoveメソッドを呼び出す
-        insect.move();
+        swallowtail.move();
 
     }
 }
