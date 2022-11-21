3b7e215678b1c222136eb055dc06bb643cdf4126
SomeyaGinji <cist.b220.g.someya@gmail.com>
2022-11-21 14:16:34 +0900

    課題3を終了

---
+++ src/lecture5/Exercise5_3.java
@@ -0,0 +1,14 @@
+package lecture5;
+
+public class Exercise5_3 {
+
+    public static void main(String[] args) {
+
+        // Insect型の変数insectにButterflyクラスのインスタンスを代入
+        Insect insect = new Butterfly();
+
+        // Butterflyクラスのmoveメソッドを呼び出す
+        insect.move();
+
+    }
+}
